03a2e93f4bfa882d7131763e4dc4dbbff3a35a37
lokn329 <lokn329@aliyun.com>
2018-11-20 23:02:15 +0800

    add tomcat

---
+++ sorb-manager-controller/src/main/java/com/fruit/sorb/hello/HelloController.java
@@ -0,0 +1,12 @@
+package com.fruit.sorb.hello;
+
+import com.fruit.sorb.Hello;
+
+public class HelloController {
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.say();
+    }
+
+}
