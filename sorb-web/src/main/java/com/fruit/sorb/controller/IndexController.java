075671308b52689119dc0c8a8a55760350bff626
lokn329 <lokn329@aliyun.com>
2019-06-15 22:30:34 +0800

    new bulid the sorb-web projection

---
+++ sorb-web/src/main/java/com/fruit/sorb/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.fruit.sorb.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName: IndexController
+ * @Description:
+ * @Author: lokn
+ * @Date: 2019/6/13 23:09
+ */
+@Controller
+public class IndexController {
+
+
+
+}
